7f4a04eec485fc943f6f65aa7b3a600422a32ec4
aponchicw <45793748+aponchicw@users.noreply.github.com>
2019-11-12 22:48:06 +0600

    uploading task6

---
+++ week6/script2.js
@@ -0,0 +1,2 @@
+let countries = ["Kazakhstan","Russia","England","France"];
+let cities_by_country = {"Kazakhstan":["Almaty","Astana","Karagandy"],"Russia":["Moscow","Saint-Petersburg","Kazan"],"England":["London","Manchester","Liverpool"],"France":["Paris","Lyon","Marseille"]};
